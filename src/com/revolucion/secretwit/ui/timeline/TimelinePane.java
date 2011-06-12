3bc776309f595a864185e7e4a24f95a47c580d0c
joshefin@gmail.com <joshefin@gmail.com@19e26eea-a116-a959-8a55-8166bf1721b2>
2011-06-12 18:01:13 +0000

    Update licence header

--- src/com/revolucion/secretwit/ui/timeline/TimelinePane.java
+++ src/com/revolucion/secretwit/ui/timeline/TimelinePane.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * Copyright (c) 2011 Željko Zirikoviæ.
+ * Copyright (c) 2011 Zeljko Zirikovic.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
+ * are made available under the terms of the GPL which 
+ * accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
  ******************************************************************************/
 package com.revolucion.secretwit.ui.timeline;
